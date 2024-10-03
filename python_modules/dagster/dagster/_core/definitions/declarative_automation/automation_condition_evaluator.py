3ab4485056427d10e27495967e6ff1a5f0d06f02
briantu <brianptu2@gmail.com>
2024-10-03 14:44:20 -0400

    Fix tests

--- python_modules/dagster/dagster/_core/definitions/declarative_automation/automation_condition_evaluator.py
+++ python_modules/dagster/dagster/_core/definitions/declarative_automation/automation_condition_evaluator.py
@@ -152,7 +152,7 @@ class AutomationConditionEvaluator:
             )
             num_evaluated += 1
 
-        for topo_level in self.asset_graph.toposorted_entity_keys:
+        for topo_level in self.asset_graph.toposorted_entity_keys_by_level:
             coroutines = [
                 _evaluate_entity_async(entity_key)
                 for entity_key in topo_level
